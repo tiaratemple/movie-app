73162fbc857c3e47d13815c001b649df442ebf67
Tiara Temple <tmtemple7@gmail.com>
2022-11-18 10:02:09 -0600

    final commit. css fixed, buttons and axios functional.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import Header from "./components/Header";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import MovieScreen from "./components/MovieScreen";
+import Watchlist from "./components/Watchlist";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
   const [list, setList] = useState([]);
   const [page, setPage] = useState(1);
+
+  const addMovie = (movie) => setList([...list, movie]);
+
+  const removeMovie = (movie) => {
+    const newState = list.filter((mov) => {
+      return mov !== movie;
+    });
+    setList(newState);
+  };
 
   const getData = () => {
     axios
@@ -32,7 +42,10 @@ function App() {
           page={page}
           setPage={setPage}
           movieList={movieList}
+          addMovie={addMovie}
+          removeMovie={removeMovie}
         />
+        <Watchlist list={list} removeMovie={removeMovie} />
       </main>
     </div>
   );
--- src/components/MovieScreen.jsx
+++ src/components/MovieScreen.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import MovieCard from './MovieCard';
 
-const MovieScreen = ({ list, page, setPage, movieList }) => {
+const MovieScreen = ({ list, page, setPage, movieList, addMovie, removeMovie }) => {
+  const decrement = () => setPage(page - 1);
+
+  const increment = () => setPage(page + 1);
+  
   const movieDisplay = movieList.map((movie, index) => {
-    return <h2>{movie.original_title}</h2>
+    return (
+      <MovieCard 
+       movie={movie} 
+       addMovie={addMovie}
+       removeMovie={removeMovie}
+       list={list}
+      />
+    )
   });
+
+
   return (
     <div className='page'>
       <h1>Tiara's Movie Theatre</h1>
       <h3>Add a movie to your watchlist!</h3>
+      <div className='btn-container'>
+        <button onClick={page !== 1 && decrement}>Previous</button>
+        <button onClick={increment}>Next</button>
+      </div>
       <div className='movie-container'>
         {movieDisplay}
       </div>
